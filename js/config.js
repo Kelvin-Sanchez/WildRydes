4e71cbd5ca20799a8899aec42ea20cef9809e478
Kelvin-Sanchez <sanchez.kelvin5376@gmail.com>
2025-02-05 19:39:03 -0800

    adding userpool and client ID

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_oBuKe7OE7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3r913t7v5finslve8hjv7t815o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
